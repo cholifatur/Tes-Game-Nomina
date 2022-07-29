48a2987896185452b59136756453bc5941a8210a
cholifatur <amangsaputra70@gmail.com>
2022-07-29 09:57:52 +0700

    Tes Game Nomina

---
+++ Assets/Script/BallBounce.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallBounce : MonoBehaviour
+{
+    Rigidbody2D rb;
+    Vector3 LastVelocity;
+    int counts = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LastVelocity = rb.velocity;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(counts < 3)
+        {
+            var speed = LastVelocity.magnitude;
+            var direction = Vector3.Reflect(LastVelocity.normalized, collision.contacts[0].normal);
+            rb.velocity = direction * Mathf.Max(speed, 0f);
+            counts++;
+        }
+        else if(counts >= 3)
+        {
+            Destroy(gameObject);
+        }
+        
+    }
+}
---
+++ Assets/Script/Shooting.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public Transform firePoint;
+  
+    public GameObject[] projectiles;
+
+    private GameObject activeProjectile; 
+
+    public float bulletforce = 20f;
+
+    private int number = 0;
+
+    private void Start()
+    {
+        activeProjectile = projectiles[0];
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown("space"))
+        {
+            Shoot();  
+        }
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            if(number > 2)
+            {
+                number = 0;
+            } 
+
+            else
+            {
+                number++;
+                Debug.Log("number");
+                activeProjectile = projectiles[number];
+            } 
+        }
+    }
+
+    void Shoot()
+    {
+        GameObject bullet = Instantiate(activeProjectile, firePoint.position, firePoint.rotation);
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.AddForce(firePoint.up * bulletforce, ForceMode2D.Impulse);
+    }
+}
